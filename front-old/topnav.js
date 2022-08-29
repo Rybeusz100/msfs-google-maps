48fae282e2277a7b229a53cc8f2f1116fde4e4de
Rybeusz100 <janzych@protonmail.com>
2022-08-29 19:55:01 +0200

    remove old code

--- front-old/topnav.js
+++
@@ -1,9 +0,0 @@
-function topnavFunction() {
-    var x = document.getElementById("topnav-id");
-    if (x.className === "topnav-class") {
-      x.className += " responsive";
-    } 
-    else {
-      x.className = "topnav-class";
-    }
-}
