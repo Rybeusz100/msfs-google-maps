92e5b289832d51ffe777832839503886574f6f32
Rybeusz100 <janzych@protonmail.com>
2023-11-07 22:43:21 +0100

    fix long timeout, add hint

--- front/src/lib/connectionStatus.ts
+++ front/src/lib/connectionStatus.ts
@@ -1,29 +1,48 @@
 import { API_URL } from './constants';
 
-enum Status {
-    SimConnected = 'hidden',
-    SimNotConnected = 'warning fa-triangle-exclamation',
-    Error = 'error fa-circle-exclamation',
+interface StatusInfo {
+    className: string;
+    hint: string;
 }
 
+const Status = {
+    SimConnected: {
+        className: 'hidden',
+        hint: '',
+    },
+    SimNotConnected: {
+        className: 'warning fa-triangle-exclamation',
+        hint: "Server is running, but it can't connect to simulator. Check if simulator is running.",
+    },
+    Error: {
+        className: 'error fa-circle-exclamation',
+        hint: "Couldn't connect to server. Check if server is running.",
+    },
+};
+
 export async function checkConnection() {
+    const setErrorTimeoutId = window.setTimeout(() => updateConnectionIcon(Status.Error), 1000);
+
     fetch(API_URL + '/status')
-        .then(async (response) => {
-            if ((await response.json())['SimConnected'] === 'true') {
+        .then((response) => response.json())
+        .then((result) => {
+            window.clearTimeout(setErrorTimeoutId);
+            if (result.SimConnected === 'true') {
                 updateConnectionIcon(Status.SimConnected);
             } else {
                 updateConnectionIcon(Status.SimNotConnected);
             }
         })
         .catch(() => {
-            updateConnectionIcon(Status.Error);
+            /* empty */
         })
         .finally(() => {
             window.setTimeout(checkConnection, 2000);
         });
 }
 
-function updateConnectionIcon(status: Status) {
+function updateConnectionIcon(status: StatusInfo) {
     const statusIcon = document.getElementById('status-icon') as HTMLElement;
-    statusIcon.className = 'fa ' + status;
+    statusIcon.className = 'fa ' + status.className;
+    statusIcon.onclick = () => window.alert(status.hint);
 }
